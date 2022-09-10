6186846ba7752c47abd7c727a680a692c6dd5fb0
HermandoThiago <hermandodev@gmail.com>
2022-09-10 00:19:21 -0300

    feat: initial structure and components

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,5 @@
+export default function Container(){
+    return (
+        <div></div>
+    );
+};
---
+++ src/components/Container/styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.section`
+    width: 100%;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 40px;
+    display: flex;
+`;
