3bf60caf64887a4cb2751ae6bf3e95c562ddd426
HermandoThiago <hermandodev@gmail.com>
2022-09-10 19:10:41 -0300

    feat: implemented cardrepositories in home

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,5 @@
+import React, { ChangeEvent, useState } from 'react';
+
 import Container from "../../components/Container/Container";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
@@ -9,7 +11,29 @@ import { StyledAuthor, StyledSearchContainer, StyledCardsContainer } from "./sty
 
 import { AiOutlineHeart, AiOutlineSearch } from 'react-icons/ai';
 
+import { getRepositories } from 'services/api';
+
+interface IRepository {
+    name: string
+    description: string
+    html_url: string
+    language: string
+    visibility: string
+    stargazers_count: number
+}
+
 export default function Home(){
+    const [user, setUser] = useState<string>('');
+    const [repositories, setRepositories] = useState<IRepository[]>([]);
+
+    const handleChangeUser = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setUser(e.target.value);
+
+    const handleSearchRepositories = async () => {
+        const response = await getRepositories(user);
+        setRepositories(response);
+        console.log(response);
+    };
+
     return (
         <>
             <Header>
@@ -20,11 +44,14 @@ export default function Home(){
                     <Input 
                         placeholder="Pesquise o usuário"
                         type="text"
+                        value={user}
+                        change={handleChangeUser}
                     />
                     <Button 
                         title="Pesquisar"
                         color="primary"
                         icon={<AiOutlineSearch />}
+                        click={handleSearchRepositories}
                     />
                     <Button 
                         title="Favoritos"
@@ -33,18 +60,16 @@ export default function Home(){
                     />      
                 </StyledSearchContainer>
                 <StyledCardsContainer>
-                    <CardRepository 
-                        title="Github-search"
-                    />
-                    <CardRepository 
-                        title="Github-search"
-                    />
-                    <CardRepository 
-                        title="Github-search"
-                    />
-                    <CardRepository 
-                        title="Github-search"
-                    />
+                    {repositories.map((repo, index) => {
+                        return (
+                            <CardRepository
+                                key={index} 
+                                title={repo.name}
+                                description={repo.description}
+                                url={repo.html_url}
+                            />
+                        )
+                    })}
                 </StyledCardsContainer>
             </Container>
             <Footer>
