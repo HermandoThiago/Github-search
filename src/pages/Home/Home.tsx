722ad49040705efa4dd42f7b8b2463610d335ccd
HermandoThiago <hermandodev@gmail.com>
2022-09-11 18:53:29 -0300

    feat: implemented cardcontainer and requests with react query

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,38 +1,49 @@
-import React, { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useState, useEffect } from 'react';
 
 import Container from "../../components/Container/Container";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
-import CardRepository from "../../components/CardRepository/CardRepository";
+import CardContainer from '../../components/CardContainer/CardContainer';
 
-import { StyledAuthor, StyledSearchContainer, StyledCardsContainer } from "./styled";
+import { StyledAuthor, StyledSearchContainer } from "./styled";
 
 import { AiOutlineHeart, AiOutlineSearch } from 'react-icons/ai';
 
 import { getRepositories } from 'services/api';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
-interface IRepository {
-    name: string
-    description: string
-    html_url: string
-    language: string
-    visibility: string
-    stargazers_count: number
-}
 
 export default function Home(){
-    const [user, setUser] = useState<string>('');
-    const [repositories, setRepositories] = useState<IRepository[]>([]);
+    const [user, setUser] = useState<string>('HermandoThiago');
+
+    const handleSearchRepositories = async (name: string) => {
+        const response = await getRepositories(name);
+        if(response.message){
+            return response.message;
+        }
+        return response;
+    };
+
+    const queryClient = useQueryClient();
+
+    const { data, isLoading, isFetching, refetch, isError, error } = useQuery(['repositories'], () => handleSearchRepositories(user), {
+        enabled: false,
+        initialData: () => {
+            const repo = queryClient
+                .getQueriesData(['repositories'])
+            
+            if(repo){
+                return repo;
+            }else {
+                return undefined;
+            }
+        }
+    })
 
     const handleChangeUser = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setUser(e.target.value);
-
-    const handleSearchRepositories = async () => {
-        const response = await getRepositories(user);
-        setRepositories(response);
-        console.log(response);
-    };
+    
 
     return (
         <>
@@ -51,7 +62,7 @@ export default function Home(){
                         title="Pesquisar"
                         color="primary"
                         icon={<AiOutlineSearch />}
-                        click={handleSearchRepositories}
+                        click={refetch}
                     />
                     <Button 
                         title="Favoritos"
@@ -59,18 +70,13 @@ export default function Home(){
                         icon={<AiOutlineHeart />}
                     />      
                 </StyledSearchContainer>
-                <StyledCardsContainer>
-                    {repositories.map((repo, index) => {
-                        return (
-                            <CardRepository
-                                key={index} 
-                                title={repo.name}
-                                description={repo.description}
-                                url={repo.html_url}
-                            />
-                        )
-                    })}
-                </StyledCardsContainer>
+                <CardContainer 
+                    loading={isLoading}
+                    fetching={isFetching}
+                    isError={isError}
+                    data={data}
+                    erro={error}
+                />
             </Container>
             <Footer>
                 Develop by @ 🎩 
